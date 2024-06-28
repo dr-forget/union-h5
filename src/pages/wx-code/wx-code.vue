426f7ea1a2284a4e545f4fa0d15dbe1ea27a1de6
Forget <fhs8825@gmail.com>
2024-06-28 20:24:33 +0800

    添加公众号二维码

---
+++ src/pages/wx-code/wx-code.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+    <image src="/static/wechat-msg.jpg" mode="widthFix" class="wx-code" />
+  </view>
+</template>
+<script setup lang="ts">
+import lineHook from '@/hooks/uni-hook';
+import useHookData from './hook';
+import { onLoad } from '@dcloudio/uni-app';
+// const { router } = lineHook
+// const {state,setData}=useHookData()
+onLoad((option) => {});
+</script>
+<style lang="scss" scoped>
+.wx-code {
+  width: 100%;
+}
+</style>
+<style>
+page {
+  background: #fff;
+}
+</style>
